a6fa4dc017176f88c500ae5a2bd4551142227023
Sandeep <sandeepnandanwar92@gmail.com>
2022-03-20 15:13:42 +0530

    ui changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,8 @@ export const Navbar = () => {
     return (
         <>
             <nav className='w3-card-4 d-flex'>
-                <img className='mx-4 my-1' src='https://freepngimg.com/thumb/chat/8-2-chat-download-png.png' height='80%' alt='image' style={{ 'borderRadius': '55%' }} />
+                {/* <img className='mx-4 my-1' src='https://freepngimg.com/thumb/chat/8-2-chat-download-png.png' height='80%' alt='image' style={{ 'borderRadius': '55%' }} /> */}
+                <image className='mx-4 my-1' src='https://freepngimg.com/thumb/chat/8-2-chat-download-png.png' height='80%' alt='image' style={{ 'borderRadius': '55%' }} />
                 <p className=' text-white live-chat'>Live Chat</p>
             </nav>
         </>
